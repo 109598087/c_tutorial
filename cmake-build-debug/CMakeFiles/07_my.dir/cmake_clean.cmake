16c5be1a2167a78f12e71c598d5fa336f9bb8a73
109598087 <t109598087@ntut.org.tw>
2021-05-14 01:00:57 +0800

    ok

---
+++ cmake-build-debug/CMakeFiles/07_my.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "07_my.exe"
+  "07_my.exe.manifest"
+  "07_my.pdb"
+  "CMakeFiles/07_my.dir/main.c.obj"
+  "lib07_my.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/07_my.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <string.h>
+
+//int main() {
+//    printf("Hello, World!\n");
+//    return 0;
+//}
+
+//int main() {
+//    // int
+//    int a = 0;
+//    scanf("%d", &a);
+//    printf("%d\n", a);
+
+//    // float
+//    float a = 0;
+//    scanf("%f", &a); // 13
+//    printf("%f\n", a); // 13.000000
+//    printf("%.1f\n", a); // 13.0
+//    printf("%.2f\n", a); // 13.00
+
+//    // float
+//    double a = 0;
+//    scanf("%lf", &a); // 13
+//    printf("%lf\n", a); // 13.000000
+//    printf("%.1lf\n", a); // 13.0
+//    printf("%.2lf\n", a); // 13.00
+
+//    // char
+//    char a = 'a'; // 單引號
+//    scanf("%c", &a);
+//    printf("%c", a);
+
+//    // String
+//    char a[10] = "hello"; // 雙引號
+//    scanf("%s", a); //這裡不用加 &
+//    printf("%s", a);
+
+//    return 0;
+//}
+
+
+//int main() {
+//    // 字串比較 strcmp
+//    // 如果返回 0 -> 兩字串一樣
+//    char str1[10] = "abc";
+//    char str2[10] = "abc";
+//    int a = strcmp(str1, str2);
+//    printf("%d", a);
+//
+//    // 如果返回不是 0 -> 兩字串不一樣
+//    char str3[10] = "abc";
+//    char str4[10] = "abcdef";
+//    int b = strcmp(str3, str4);
+//    printf("%d", b);
+//}
+
+int main() {
+    
+}
+
